154a61235873d88f0860a327cba71b6246632d16
potagashi <49604132+watagashi0619@users.noreply.github.com>
2022-03-11 04:48:32 +0000

    feat: add table (room and room_member)

--- schema.sql
+++ schema.sql
@@ -1,9 +1,23 @@
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `token` varchar(255) DEFAULT NULL,
-  `leader_card_id` int DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `leader_card_id` int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
+);
+
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE `room` (
+  `room_id` bigint NOT NULL AUTO_INCREMENT,
+  `live_id` int NOT NULL,
+  `joined_user_count` int NOT NULL,
+  PRIMARY KEY (`room_id`)
+);
+
+DROP TABLE IF EXISTS `room_member`;
+CREATE TABLE `room_member` (
+  `room_id` bigint NOT NULL,
+  `user_id` int NOT NULL
 );
